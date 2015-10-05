19fa9a7774582a72006331324d44a1f88eac27db
Christopher Muller <inbox@christophermuller.net>
2015-10-05 21:25:06 +1100

    Using object loop.

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ var github = githubhook({host: config.server.host, port: config.server.port, pat
 github.listen();
 
 console.log('Starting listener loop.');
-config.deployments.forEach(function(deployment) {
-	console.log('Setting listener for '+deployment.repo);
-	github.on('*', function (event, repo, ref, data) {
-
-		var git = require('simple-git')(deployment.localPath);
-	
-		if(ref == "refs/heads/"+deployment.branch && repo == deployment.repo) {
-			console.log('Pulling data for '+deployment.repo);
-			git.pull();
-		}
-	});
-}, this);
+for(var deploymentIndex in config.deployments) { 
+	if (config.deployments.hasOwnProperty(deploymentIndex)) {
+		var deployment = config.deployments[deploymentIndex];
+		
+		console.log('Setting listener for '+deployment.repo);
+		github.on('*', function (event, repo, ref, data) {
+			
+			var git = require('simple-git')(deployment.localPath);
+			
+			if(ref == "refs/heads/"+deployment.branch && repo == deployment.repo) {
+				console.log('Pulling data for '+deployment.repo);
+				git.pull();
+			}
+		});
+	}
+}
