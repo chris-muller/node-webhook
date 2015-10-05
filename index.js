f90448265e567f190df1e13587f960b72bd3c9a6
Christopher Muller <inbox@christophermuller.net>
2015-10-05 21:14:46 +1100

    Updated with debug output.

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ for (var i = 0; i < config.deployments.length; i++) {
 	var deployment = config.deployments[i];
 
 	github.on('push', function (repo, ref, data) {
-
+		console.log('Setting listener for '+deployment.repo);
 		var git = require('simple-git')(deployment.localPath);
 	
 		if(ref == "refs/heads/"+deployment.branch && repo == deployment.repo) {
+			console.log('Pulling data for '+deployment.repo);
 			git.pull();
 		}
 	});
