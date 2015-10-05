f3ec40dd56a8327a4bf5546ad820be34a4fa787f
Christopher Muller <inbox@christophermuller.net>
2015-10-05 21:33:30 +1100

    More debug info

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ for(var deploymentIndex in config.deployments) {
 	if (config.deployments.hasOwnProperty(deploymentIndex)) {
 		var deployment = config.deployments[deploymentIndex];
 		
-		console.log('Setting listener for '+deployment.repo);
+		console.log('Setting listener for '+deployment.repo + ":" + deploymentIndex);
 		github.on('*', function (event, repo, ref, data) {
 			console.log('Listener called for '+deployment.repo);
 			
@@ -20,7 +20,7 @@ for(var deploymentIndex in config.deployments) {
 				console.log('Pulling data for '+deployment.repo);
 				git.pull();
 			} else {
-				console.log('Skipped: repo '+deployment.repo + " - branch " + deployment.branch);
+				console.log(deploymentIndex + 'skipped: repo '+deployment.repo + " - branch " + deployment.branch);
 			}
 		});
 	}
